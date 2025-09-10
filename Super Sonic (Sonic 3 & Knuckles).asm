c8707951e358067b825fb5386f6416c0bab23cbd
DMP <dankscope2091@gmail.com>
2025-09-10 00:55:45 -0400

    Update Super Sonic (Sonic 3 & Knuckles).asm

--- Super Sonic (Sonic 3 & Knuckles).asm
+++ Super Sonic (Sonic 3 & Knuckles).asm
@@ -77,13 +77,13 @@ Snd_Supersonic_FM2:
 	smpsPan             panRight, $00
 	dc.b	nFs4, nAb4, nA4, nB4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $05
+	smpsAlterVol        $0A
 
 Snd_Supersonic_Loop03:
 	smpsCall            Snd_Supersonic_Call03
 	smpsCall            Snd_Supersonic_Call04
 	smpsLoop            $00, $02, Snd_Supersonic_Loop03
-	smpsAlterVol        $FB
+	smpsAlterVol        $F6
 	smpsJump            Snd_Supersonic_FM2
 
 Snd_Supersonic_Call03:
@@ -106,7 +106,7 @@ Snd_Supersonic_Loop0B:
 	smpsReturn
 
 Snd_Supersonic_Call04:
-	smpsAlterVol        $FB
+	smpsAlterVol        $F6
 	smpsPan             panRight, $00
 	dc.b	nE5, $06, nD5, nCs5, nB4
 	smpsPan             panLeft, $00
@@ -116,7 +116,7 @@ Snd_Supersonic_Call04:
 	smpsPan             panLeft, $00
 	dc.b	nA4, nAb4, nFs4, nE4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $05
+	smpsAlterVol        $0A
 	smpsReturn
 
 ; FM3 Data
