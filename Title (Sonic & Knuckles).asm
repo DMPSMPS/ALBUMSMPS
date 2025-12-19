e8b176e747895bc193915782cb0497ab5b4d513e
DMP <dankscope2091@gmail.com>
2025-12-19 17:41:04 -0500

    Update Title (Sonic & Knuckles).asm

--- Title (Sonic & Knuckles).asm
+++ Title (Sonic & Knuckles).asm
@@ -79,10 +79,11 @@ Snd_Title_Loop58:
 	dc.b	nA4, $3C, nRst, $0C, nCs5, $0C, nD5, $0C, nE5, $0C, nFs5, $0C
 	dc.b	nFs5, $18, nFs5, $0C, nE5, $18, nD5, $0C, nE5, $6C, nFs5, $0C
 	dc.b	nFs5, $18, nFs5, $0C, nE5, $18, nD5, $0C, nE5, $3C, nRst, $18
-	dc.b	nB4, $0C, nCs5, $0C, nD5, $18, nCs5, $0C, nD5, $0C, nRst, $0C
-	dc.b	nA5, $0C, nRst, $0C, nFs5, $48, nRst, $0C, nD5, $0C, nFs5, $0C
-	dc.b	nE5, $10, nCs5, $10, nB4, $10, nCs5, $04, nRst, $08, nE5, $18
-	dc.b	nE5, $10, nRst, $32
+	dc.b	nB4, $0C, nCs5, $0C, nD5, $18, nCs5, $0C, nD5, $0C, nRst, $0C, nA5, $0C
+	dc.b	nRst, $0C, nFs5, $48, nRst, $0C, nD5, $0C, nFs5, $0C, nE5, $10
+	dc.b	nCs5, $10, nB4, $10, nCs5, $04, nRst, $08, nE5, $18, nE5, $12
+	dc.b	nRst, $30
+
 
 Snd_Title_Loop59:
 	dc.b	nB4, $06, nCs5, $06, nE5, $06
