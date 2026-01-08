be1a82f5866f0b48b59d7247eb87d67ff4a5358b
DMP <dankscope2091@gmail.com>
2026-01-07 20:31:52 -0500

    Replace TightSnare with ElectricTom sounds

--- Sonic 4 Episode 1/snd_sng_z2a2 (Mobile).asm
+++ Sonic 4 Episode 1/snd_sng_z2a2 (Mobile).asm
@@ -17,8 +17,8 @@ snd_sng_z2a2_Header:
 ; DAC Data
 snd_sng_z2a2_DAC:
 	dc.b	dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C, dSnareS3, $06, dKickS3, $0C, dKickS3, $06
-	dc.b	dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dTightSnare, $06, dTightSnare, $06, dKickS3, $06
-	dc.b	dSnareS3, $0C, dTightSnare, $06, dLooserSnare, $06, dKickS3, $0C, dSnareS3, $0C, dSnareS3, $06
+	dc.b	dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dElectricHighTom, $06, dElectricHighTom, $06, dKickS3, $06
+	dc.b	dSnareS3, $0C, dElectricHighTom, $06, dElectricFloorTom, $06, dKickS3, $0C, dSnareS3, $0C, dSnareS3, $06
 	dc.b	dKickS3, $06, dSnareS3, $06, dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C
 	dc.b	dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C
 	dc.b	dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C, dSnareS3, $06, dSnareS3, $06
@@ -33,8 +33,8 @@ snd_sng_z2a2_DAC:
 	dc.b	dKickS3, $0C, dSnareS3, $0C, dKickS3, $0C, dSnareS3, $0C, dKickS3, $06, dSnareS3, $0C
 	dc.b	dSnareS3, $06, dKickS3, $06, dSnareS3, $06, dSnareS3, $06, dSnareS3, $06, dKickS3, $0C
 	dc.b	dSnareS3, $0C, dKickS3, $0C, dSnareS3, $06, dKickS3, $0C, dKickS3, $06, dSnareS3, $0C
-	dc.b	dKickS3, $0C, dSnareS3, $0C, dTightSnare, $06, dTightSnare, $06, dKickS3, $06, dSnareS3, $0C
-	dc.b	dTightSnare, $06, dLooserSnare, $06, dKickS3, $0C, dSnareS3, $0C, dSnareS3, $06, dKickS3, $06
+	dc.b	dKickS3, $0C, dSnareS3, $0C, dElectricHighTom, $06, dElectricHighTom, $06, dKickS3, $06, dSnareS3, $0C
+	dc.b	dElectricHighTom, $06, dElectricFloorTom, $06, dKickS3, $0C, dSnareS3, $0C, dSnareS3, $06, dKickS3, $06
 	dc.b	dSnareS3, $06, dSnareS3, $3C
 	smpsStop
 
