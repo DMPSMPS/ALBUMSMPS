261f074a1ba7f4cb5b1c1a3c7c5438525cbeb4f0
DMP <dankscope2091@gmail.com>
2026-01-09 17:26:27 -0500

    Update snd_sng_title.asm

--- Sonic 4 Episode 1/snd_sng_title.asm
+++ Sonic 4 Episode 1/snd_sng_title.asm
@@ -229,7 +229,7 @@ snd_sng_title_Voices:
 	smpsVcFeedback      $07
 	smpsVcUnusedBits    $00
 	smpsVcDetune        $01, $05, $02, $01
-	smpsVcCoarseFreq    $02, $03, $01, $02
+	smpsVcCoarseFreq    $02, $03, $01, $04
 	smpsVcRateScale     $00, $00, $00, $00
 	smpsVcAttackRate    $0F, $14, $14, $12
 	smpsVcAmpMod        $00, $00, $00, $00
