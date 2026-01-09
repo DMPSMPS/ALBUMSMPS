176758226fd4a4b02384cf157dfb53e211563891
DMP <dankscope2091@gmail.com>
2026-01-09 03:45:34 -0500

    Update snd_jin_invincible.asm

--- Sonic 4 Episode 1/snd_jin_invincible.asm
+++ Sonic 4 Episode 1/snd_jin_invincible.asm
@@ -213,7 +213,7 @@ snd_jin_invincible_Voices:
 	smpsVcFeedback      $07
 	smpsVcUnusedBits    $00
 	smpsVcDetune        $01, $05, $02, $01
-	smpsVcCoarseFreq    $02, $04, $01, $02
+	smpsVcCoarseFreq    $02, $03, $01, $02
 	smpsVcRateScale     $00, $00, $00, $00
 	smpsVcAttackRate    $0F, $14, $14, $12
 	smpsVcAmpMod        $00, $00, $00, $00
