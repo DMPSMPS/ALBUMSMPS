229e3687e2626c6d9e8f6f516adbfb21f8b566fb
DMP <dankscope2091@gmail.com>
2026-01-10 04:13:24 -0500

    Update snd_sng_final.asm

--- Sonic 4 Episode 1/snd_sng_final.asm
+++ Sonic 4 Episode 1/snd_sng_final.asm
@@ -10,8 +10,8 @@ snd_sng_final_Header:
 	smpsHeaderFM        snd_sng_final_FM3,	$00, $1E
 	smpsHeaderFM        snd_sng_final_FM4,	$00, $08
 	smpsHeaderFM        snd_sng_final_FM5,	$00, $08
-	smpsHeaderPSG       snd_sng_final_PSG1,	$0C, $03, $00, $00
-	smpsHeaderPSG       snd_sng_final_PSG2,	$0C, $02, $00, $00
+	smpsHeaderPSG       snd_sng_final_PSG1,	$0C, $03, $00, sTone_0A
+	smpsHeaderPSG       snd_sng_final_PSG2,	$0C, $03, $00, sTone_17
 
 ; DAC Data
 snd_sng_final_DAC:
