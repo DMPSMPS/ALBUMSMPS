351efa8c03718928d9c48b665914a02f9ecebbe5
DMP <dankscope2091@gmail.com>
2026-01-23 02:33:31 -0500

    Update snd_jin_invincible.asm

--- Sonic 4 Episode 1/snd_jin_invincible.asm
+++ Sonic 4 Episode 1/snd_jin_invincible.asm
@@ -111,7 +111,7 @@ snd_jin_invincible_FM3:
 	smpsSetvoice        $01
 	smpsAlterNote	    $FD
 	smpsPan             panRight, $00
-	dc.b	nRst, $0C
+	dc.b	nRst, $06
 	smpsJump		snd_jin_invincible_FM2INIT
 
 ; FM5 Data
