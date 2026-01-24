e7dc0f1472787de6aa525c8db96102db8c0307cb
DMP <dankscope2091@gmail.com>
2026-01-24 03:34:18 -0500

    FINALIZED 1/24/25

--- Sonic 4 Episode 1/snd_sng_z1a1.asm
+++ Sonic 4 Episode 1/snd_sng_z1a1.asm
@@ -121,7 +121,7 @@ snd_sng_z1a1_FM3INIT:
 ; FM4 Data
 snd_sng_z1a1_FM4:
 	smpsModSet          $07, $01, $03, $05
-	smpsSetvoice        $01
+	smpsSetvoice        $02
 	smpsAlterNote       $FD
 	;smpsPan             panRight, $00
 	dc.b	nRst, $06
