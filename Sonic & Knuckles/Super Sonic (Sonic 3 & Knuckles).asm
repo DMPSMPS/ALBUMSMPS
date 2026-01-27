5d8ba4ff97cf8809c8464db7f7774cd627d6c4d6
DMP <dankscope2091@gmail.com>
2026-01-27 02:20:40 -0500

    Custom Organ Voice

--- Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
+++ Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
@@ -367,14 +367,14 @@ Snd_Supersonic_Voices:
 	smpsVcTotalLevel    $00, $27, $28, $17
 
 ;	Voice $03
-;	$2C
-;	$71, $72, $33, $33, 	$1F, $1F, $1F, $1F, 	$00, $00, $00, $00
-;	$00, $00, $00, $00, 	$0F, $0F, $0F, $0F, 	$1E, $80, $23, $82
-	smpsVcAlgorithm     $04
+;	$2E
+;	$33, $74, $32, $71, 	$1F, $1F, $1F, $1F, 	$00, $00, $00, $00
+;	$00, $00, $00, $00, 	$0F, $0F, $0F, $0F, 	$23, $02, $00, $00
+	smpsVcAlgorithm     $06
 	smpsVcFeedback      $05
 	smpsVcUnusedBits    $00
-	smpsVcDetune        $03, $03, $07, $07
-	smpsVcCoarseFreq    $03, $03, $02, $01
+	smpsVcDetune        $07, $03, $07, $03
+	smpsVcCoarseFreq    $01, $02, $04, $03
 	smpsVcRateScale     $00, $00, $00, $00
 	smpsVcAttackRate    $1F, $1F, $1F, $1F
 	smpsVcAmpMod        $00, $00, $00, $00
@@ -382,7 +382,7 @@ Snd_Supersonic_Voices:
 	smpsVcDecayRate2    $00, $00, $00, $00
 	smpsVcDecayLevel    $00, $00, $00, $00
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $02, $23, $00, $1E
+	smpsVcTotalLevel    $00, $00, $02, $23
 
 ;	Voice $04
 ;	$36
