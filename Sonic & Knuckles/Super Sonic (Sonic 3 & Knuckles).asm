fd85b39666c1a892f87e032b37b119f5ded4a543
DMP <dankscope2091@gmail.com>
2026-01-31 21:16:18 -0500

    Update Super Sonic (Sonic 3 & Knuckles).asm

--- Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
+++ Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
@@ -316,7 +316,7 @@ Snd_Supersonic_Voices:
 ;	Voice $00
 ;	$32
 ;	$70, $76, $30, $71, 	$1F, $95, $1F, $1F, 	$0E, $0F, $0E, $00
-;	$07, $06, $00, $04, 	$2F, $4F, $4F, $2F, 	$29, $1A, $1A, $04
+;	$07, $06, $00, $04, 	$2F, $4F, $4F, $2F, 	$2D, $1E, $18, $02
 	smpsVcAlgorithm     $02
 	smpsVcFeedback      $06
 	smpsVcUnusedBits    $00
@@ -329,7 +329,7 @@ Snd_Supersonic_Voices:
 	smpsVcDecayRate2    $00, $00, $00, $00
 	smpsVcDecayLevel    $02, $04, $04, $02
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $04, $1A, $1A, $29
+	smpsVcTotalLevel    $02, $18, $1E, $2D
 
 ;	Voice $01
 ;	$3D
