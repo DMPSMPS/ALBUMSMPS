f832195a34bd2ffe6637e02f6d68a4d9f5b3482e
DMP <dankscope2091@gmail.com>
2026-02-20 20:25:26 -0500

    SPECTROGRAM VERIFIED

--- Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
+++ Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
@@ -77,13 +77,13 @@ Snd_Supersonic_FM2:
 	smpsPan             panRight, $00
 	dc.b	nFs4, nAb4, nA4, nB4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $0A
+	smpsAlterVol        $07
 
 Snd_Supersonic_Loop03:
 	smpsCall            Snd_Supersonic_Call03
 	smpsCall            Snd_Supersonic_Call04
 	smpsLoop            $00, $02, Snd_Supersonic_Loop03
-	smpsAlterVol        $F6
+	smpsAlterVol        $F9
 	smpsJump            Snd_Supersonic_FM2
 
 Snd_Supersonic_Call03:
@@ -105,7 +105,7 @@ Snd_Supersonic_Loop0B:
 	smpsReturn
 
 Snd_Supersonic_Call04:
-	smpsAlterVol        $F6
+	smpsAlterVol        $F9
 	smpsPan             panRight, $00
 	dc.b	nE5, $06, nD5, nCs5, nB4
 	smpsPan             panLeft, $00
@@ -115,7 +115,7 @@ Snd_Supersonic_Call04:
 	smpsPan             panLeft, $00
 	dc.b	nA4, nAb4, nFs4, nE4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $0A
+	smpsAlterVol        $07
 	smpsReturn
 
 ; FM3 Data
