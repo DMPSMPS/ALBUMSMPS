f6bbae55e048c551a8ee15feaf6921ead67d668c
DMP <dankscope2091@gmail.com>
2026-02-21 15:13:03 -0500

    Final

--- Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
+++ Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
@@ -77,13 +77,13 @@ Snd_Supersonic_FM2:
 	smpsPan             panRight, $00
 	dc.b	nFs4, nAb4, nA4, nB4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $0A
+	smpsAlterVol        $05
 
 Snd_Supersonic_Loop03:
 	smpsCall            Snd_Supersonic_Call03
 	smpsCall            Snd_Supersonic_Call04
 	smpsLoop            $00, $02, Snd_Supersonic_Loop03
-	smpsAlterVol        $F6
+	smpsAlterVol        $FB
 	smpsJump            Snd_Supersonic_FM2
 
 Snd_Supersonic_Call03:
@@ -105,7 +105,7 @@ Snd_Supersonic_Loop0B:
 	smpsReturn
 
 Snd_Supersonic_Call04:
-	smpsAlterVol        $F6
+	smpsAlterVol        $FB
 	smpsPan             panRight, $00
 	dc.b	nE5, $06, nD5, nCs5, nB4
 	smpsPan             panLeft, $00
@@ -115,7 +115,7 @@ Snd_Supersonic_Call04:
 	smpsPan             panLeft, $00
 	dc.b	nA4, nAb4, nFs4, nE4
 	smpsPan             panCenter, $00
-	smpsAlterVol        $0A
+	smpsAlterVol        $05
 	smpsReturn
 
 ; FM3 Data
@@ -316,9 +316,9 @@ Snd_Supersonic_Voices:
 	smpsVcRateScale     $00, $00, $00, $00
 	smpsVcAttackRate    $0F, $0F, $14, $1F
 	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $00, $00, $00, $00
-	smpsVcDecayRate2    $00, $00, $00, $00
-	smpsVcDecayLevel    $00, $00, $00, $00
+	smpsVcDecayRate1    $0C, $05, $0F, $0E
+	smpsVcDecayRate2    $07, $06, $06, $07
+	smpsVcDecayLevel    $02, $02, $02, $00
 	smpsVcReleaseRate   $09, $09, $09, $00
 	smpsVcTotalLevel    $00, $00, $00, $1D
 
