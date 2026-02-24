a890896e86a33a3be100e0e113ba8a030a107f6a
DMP <dankscope2091@gmail.com>
2026-02-23 19:31:56 -0500

    Final

--- Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
+++ Sonic & Knuckles/Super Sonic (Sonic 3 & Knuckles).asm
@@ -134,7 +134,7 @@ Snd_Supersonic_Call06:
 	smpsReturn
 
 Snd_Supersonic_Call07:
-	smpsAlterVol        $1C
+	smpsAlterVol        $1D
 	smpsSetvoice        $05
 	smpsPan             panCenter, $00
 	dc.b	nRst, $60, nA3, $03
